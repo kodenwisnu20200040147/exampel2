83d2853ebf620bfb46a27b65eefd7ced8f70aa09
Npandan <kodenwisnu09@gmail.com>
2023-11-01 12:37:07 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ListV());
+}
+
+class ListV extends StatelessWidget {
+  final List<Widget> w = [];
+  ListV(){
+    for(int i=1;i<50;i++){
+      w.add(
+        ListTile(
+          leading: Icon(Icons.favorite,color: Colors.amber),
+          title: Text("List $i"),
+          subtitle: Text("learning lis view"),
+        )
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home: Scaffold(
+      appBar: AppBar(title:Text("Llost View")),
+        body: ListView(
+          children:w,
+        )
+      )
+    );
+  }
+}
+
